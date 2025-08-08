e21e71811012557836ec62f674d2ffbf04dad5ff
Tarunchintakunta <tarunchintakunta@gmail.com>
2025-08-09 00:24:31 +0530

    Integrate MotionPlugin into main.js and import main.css for enhanced animations and styling.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/main.css'
+import { MotionPlugin } from '@vueuse/motion'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(MotionPlugin)
+app.mount('#app')
